2277e75c2bde4fba68f82daec4e821ee6d480b65
pete <user.email>
2023-10-10 08:49:16 +0300

    spring init

---
+++ src/main/java/ro/uvt/info/splabbunea/SpLabBuneaApplication.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.splabbunea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLabBuneaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLabBuneaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/uvt/info/splabbunea/SpLabBuneaApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.splabbunea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpLabBuneaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
