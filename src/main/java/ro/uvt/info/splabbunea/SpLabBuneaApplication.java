e9f3ba49c9687fa3d4623c97fc501ab6f5982b0c
pete <user.email>
2023-11-07 09:26:55 +0200

    lab6

--- src/main/java/ro/uvt/info/splabbunea/SpLabBuneaApplication.java
+++ src/main/java/ro/uvt/info/splabbunea/SpLabBuneaApplication.java
@@ -2,34 +2,50 @@ package ro.uvt.info.splabbunea;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.ApplicationContext;
+import ro.uvt.info.splabbunea.injection.ClientComponent;
+import ro.uvt.info.splabbunea.injection.SingletonComponent;
+import ro.uvt.info.splabbunea.injection.TransientComponent;
+import ro.uvt.info.splabbunea.controllers.HelloWorld;
 @SpringBootApplication
 public class SpLabBuneaApplication {
-
     public static void main(String[] args) {
-        IAlignStrategy alignLeft = new AlignLeft();
-//        Book noapteBuna = new Book("Noapte buna, copii!");
-//        Author rpGheo = new Author("Radu Pavel Gheo");
-//        noapteBuna.addAuthor(rpGheo);
-//        Sections cap1 = new Sections("Capitolul 1");
-//        Sections cap11 = new Sections("Capitolul 1.1");
-//        Sections cap111 = new Sections("Capitolul 1.1.1");
-//        Sections cap1111 = new Sections("Subchapter 1.1.1.1");
-//        noapteBuna.addContent(new Paragraph("Multumesc celor care ..."));
-//        noapteBuna.addContent(cap1);
-//        cap1.add(new Paragraph("Moto capitol"));
-//        cap1.add(cap11);
-//        cap11.add(new Paragraph("Text from subchapter 1.1"));
 //
-//        cap11.add(cap111);
-//        cap111.add(new Paragraph("Text from subchapter 1.1.1"));
-//        cap111.add(cap1111);
-//        cap1111.add(new Image("Image subchapter 1.1.1.1"));
-//        noapteBuna.print();
-        Paragraph paragraph = new Paragraph("Multumesc celor care nu stiu ce au facut, dar au facut ceva");
-        paragraph.print();
-        paragraph.setAlignStrategy(alignLeft);
-        paragraph.print();
+// Run this main function and inspect the output console
+// to learn about
+// the lifecycle of objects within
+// Spring Dependency Injection Context
+//
+// Gets a handle of dependency injection context
+        ApplicationContext context =
+                SpringApplication.run(SpLabBuneaApplication.class, args);
+// Gets an instance of TransientComponent from the DI context
+        TransientComponent transientBean =
+                context.getBean(TransientComponent.class);
+        transientBean.operation();
+// Note that every time an instance is required,
+// the DI context creates a new one
+        transientBean = context.getBean(TransientComponent.class);
+        transientBean.operation();
+// Gets an instance of SingletonComponent from the DI context
+// Note that the unique instance was created while
+// application was loaded, before creating
+// the transient instances
+        SingletonComponent singletonBean =
+                context.getBean(SingletonComponent.class);
+        singletonBean.operation();
+// Note that every time an instance is required,
+// the DI returns the same unique one
+        singletonBean = context.getBean(SingletonComponent.class);
+        singletonBean.operation();
+// Gets an instance of another class that
+// requires singleton/transient components
+// Note where this instance was created and what beans
+// were used to initialize it
+        ClientComponent c = context.getBean(ClientComponent.class);
+        c.operation();
+// One can also request an instance from DI context by name
+        c = (ClientComponent)context.getBean(ClientComponent.class);
+        c.operation();
     }
-
 }
